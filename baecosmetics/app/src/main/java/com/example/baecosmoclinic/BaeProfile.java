049b8b3b40ec2535ae54ed67925d3435a4fb8c48
hyeparis <jjubob93@gmail.com>
2020-02-11 18:29:06 -0800

    bae profile update

--- baecosmetics/app/src/main/java/com/example/baecosmoclinic/BaeProfile.java
+++ baecosmetics/app/src/main/java/com/example/baecosmoclinic/BaeProfile.java
@@ -24,50 +24,30 @@ public class BaeProfile extends AppCompatActivity {
         final TextView tv4 = (TextView) findViewById(R.id.profileTextView4);
 
 
-        tv1.setText("Education & Training\n");
-        tv2.setText(
-                "M.D. Saint Louis University - School of Medicine\n" +
-                        "Residency, Plastic Surgery - UMKC/Truman Medical Center\n" +
-                        "Fellowship, Hand Surgery - UMKC/KU\n" +
-                        "\n" +
-                        "Certified by The American Board of Plastic Surgery (ABPS)\n");
-        tv3.setText("Asian Cosmetic Surgery Specialist\n");
-        tv4.setText("\tThe creation of an aesthetically pleasing Asian face requires thorough knowledge of the anatomy of the Asian facial skeleton as well as an appreciation of the Asian ideals of beauty.\n\n" +
-                "\tSymmetrical eyelid crease with smooth, firmly attached skin overlying the tarsus, and mobile, loosely attached skin above the level of the crease is aesthetically appealing in Asians.\n\n" +
-                "\tAn appreciation of the Asian concept of beauty is required in order to practice Asian cosmetic surgery. In addition to the surgeon's aesthetic sense, imagination, and creativity, the surgeon's training and experience in plastic and reconstructive surgery are absolute requirements."
-            );
+        tv1.setText(getString(R.string.education));
+        tv2.setText(getString(R.string.education_detail));
+        tv3.setText(getString(R.string.intro));
+        tv4.setText(getString(R.string.intro_detail));
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 if(tab.getPosition()==0){
 
-                    tv1.setText("Education & Training\n");
-                    tv2.setText(
-                            "M.D. Saint Louis University - School of Medicine\n" +
-                            "Residency, Plastic Surgery - UMKC/Truman Medical Center\n" +
-                            "Fellowship, Hand Surgery - UMKC/KU\n" +
-                            "\n" +
-                            "Certified by The American Board of Plastic Surgery (ABPS)\n");
-                    tv3.setText("Asian Cosmetic Surgery Specialist\n");
-                    tv4.setText("\tThe creation of an aesthetically pleasing Asian face requires thorough knowledge of the anatomy of the Asian facial skeleton as well as an appreciation of the Asian ideals of beauty.\n\n" +
-                            "\tSymmetrical eyelid crease with smooth, firmly attached skin overlying the tarsus, and mobile, loosely attached skin above the level of the crease is aesthetically appealing in Asians.\n\n" +
-                            "\tAn appreciation of the Asian concept of beauty is required in order to practice Asian cosmetic surgery. In addition to the surgeon's aesthetic sense, imagination, and creativity, the surgeon's training and experience in plastic and reconstructive surgery are absolute requirements."
-                    );
 
+                    tv1.setText(getString(R.string.education));
+                    tv2.setText(getString(R.string.education_detail));
+                    tv3.setText(getString(R.string.intro));
+                    tv4.setText(getString(R.string.intro_detail));
 
                 }
                 else if(tab.getPosition()==1){
 
-                    tv1.setText("Location\n");
-                    tv2.setText("BAE Cosmetic Surgery\n" +
-                            "Asia Aesthetica\n" +
-                            "2727 W. Olympic Bl., Suite 304\n" +
-                            "Los Angeles, CA  90006\n");
+                    tv1.setText(getString(R.string.location));
+                    tv2.setText(getString(R.string.location_detail));
 
                     tv3.setText("");
-                    tv4.setText("Tel. (213) 384-7555\n" +
-                            "Fax. (213) 738-8798\n");
+                    tv4.setText(getString(R.string.contact_detail));
 
                 }
             }
