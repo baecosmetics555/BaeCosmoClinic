8c886c1aa88d53ec86fc93ccd1363c3900f67e7d
sanjayatlive <sanjayatlive@users.noreply.github.com>
2020-02-23 20:45:43 -0800

    Notification added

--- baecosmetics/app/src/main/java/com/example/baecosmoclinic/NotificationActivity.java
+++ baecosmetics/app/src/main/java/com/example/baecosmoclinic/NotificationActivity.java
@@ -1,14 +1,59 @@
 package com.example.baecosmoclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.douglas.baecosmoclinic.adapter.NotificationRecyclerAdapter;
+import com.douglas.baecosmoclinic.adapter.RecyclerViewAdapter;
+import com.douglas.bean.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NotificationActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<Service> serviceList ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        getIds();
+
+        serviceList = new ArrayList<>();
+        serviceList.add(new Service("The Vegitarian","Categorie Service","Description book",R.drawable.thevigitarian));
+        serviceList.add(new Service("The Wild Robot","Categorie Service","Description book",R.drawable.thewildrobot));
+        serviceList.add(new Service("Maria Semples","Categorie Service","Description book",R.drawable.mariasemples));
+        serviceList.add(new Service("The Martian","Categorie Service","Description book",R.drawable.themartian));
+        serviceList.add(new Service("He Died with...","Categorie Service","Description book",R.drawable.hediedwith));
+        serviceList.add(new Service("The Vegitarian","Categorie Service","Description book",R.drawable.thevigitarian));
+        serviceList.add(new Service("The Wild Robot","Categorie Service","Description book",R.drawable.thewildrobot));
+        serviceList.add(new Service("Maria Semples","Categorie Service","Description book",R.drawable.mariasemples));
+        serviceList.add(new Service("The Martian","Categorie Service","Description book",R.drawable.themartian));
+        serviceList.add(new Service("He Died with...","Categorie Service","Description book",R.drawable.hediedwith));
+        serviceList.add(new Service("The Vegitarian","Categorie Service","Description book",R.drawable.thevigitarian));
+        serviceList.add(new Service("The Wild Robot","Categorie Service","Description book",R.drawable.thewildrobot));
+        serviceList.add(new Service("Maria Semples","Categorie Service","Description book",R.drawable.mariasemples));
+        serviceList.add(new Service("The Martian","Categorie Service","Description book",R.drawable.themartian));
+        serviceList.add(new Service("He Died with...","Categorie Service","Description book",R.drawable.hediedwith));
+
+
+        NotificationRecyclerAdapter myAdapter = new NotificationRecyclerAdapter(this,serviceList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter);
+
     }
+
+    public void getIds()
+    {
+        recyclerView = findViewById(R.id.recyclerView);
+
+    }
+
 }
