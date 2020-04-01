d10acb2b2582e8427f0a88f0a1dffdee19ea1283
sanjayatlive <sanjayatlive@users.noreply.github.com>
2020-03-31 19:11:01 -0700

    see appt

--- baecosmetics/app/src/main/java/com/example/baecosmoclinic/UserProfile.java
+++ baecosmetics/app/src/main/java/com/example/baecosmoclinic/UserProfile.java
@@ -1,16 +1,25 @@
 package com.example.baecosmoclinic;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bean.TimeSlots;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +31,13 @@ public class UserProfile extends AppCompatActivity {
     String userEmail;
     TextView txtAccount;
 
+    Button btnSeeAppoingment;
+
+    FirebaseDatabase mDatabase;
+    DatabaseReference mDatabaseReference;
+
+    Boolean empty = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +46,10 @@ public class UserProfile extends AppCompatActivity {
        userEmail = i.getStringExtra("userEmail");
 
         txtAccount = findViewById(R.id.txtAccount);
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference();
+
+        btnSeeAppoingment = findViewById(R.id.btnSeeAppoingment);
 
        // String name = FirebaseAuth.getInstance().getCurrentUser().getEmail();
       //  System.out.println("======================================" + name);
@@ -71,5 +91,86 @@ public class UserProfile extends AppCompatActivity {
                 }
             }
         });
+
+
+
+
+        btnSeeAppoingment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                empty = true;
+                //empty[0] = true;
+
+                SharedPreferences sp = getSharedPreferences("userinfo" , Context.MODE_PRIVATE);
+                String email  = sp.getString("email","null");
+                final String usn=  email.trim().split("@")[0];
+
+
+                //  DatabaseReference mDatabaseReferenceN;
+                mDatabaseReference = mDatabase.getReference().child("appointment").child("days");
+
+                mDatabaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        TimeSlots friday =    dataSnapshot.child("friday").child("timeslots").getValue(TimeSlots.class);
+                        TimeSlots monday =    dataSnapshot.child("monday").child("timeslots").getValue(TimeSlots.class);
+                        TimeSlots tuesday =    dataSnapshot.child("tuesday").child("timeslots").getValue(TimeSlots.class);
+                        TimeSlots wednesday =    dataSnapshot.child("wednesday").child("timeslots").getValue(TimeSlots.class);
+                        TimeSlots thursday =    dataSnapshot.child("thursday").child("timeslots").getValue(TimeSlots.class);
+
+                        TimeSlots booking=null;
+
+                        if(friday.getUsername().equals(usn))
+                        {
+                            empty = false;
+                            booking = friday;}
+                        else if(monday.getUsername().equals(usn))
+                        {
+                            empty = false;
+                            booking = monday;}
+                        else if(tuesday.getUsername().equals(usn))
+                        {
+                            empty = false;
+                            booking = tuesday;}
+                        else if(wednesday.getUsername().equals(usn))
+                        {
+                            empty = false;
+                            booking = wednesday;}
+                        else if(thursday.getUsername().equals(usn))
+                        {
+                            empty = false;
+                            booking = thursday;}
+
+                        if(empty == true)
+                        {
+                            Snackbar snackbar = Snackbar
+                                    .make(findViewById(R.id.coordinatorLayout), "No Appointments Found", Snackbar.LENGTH_LONG);
+                            snackbar.show();
+
+                            //   startActivity(new Intent(ScheduleActivity.this, ViewClientAppointmentsActivity.class));
+
+
+                        }else {
+
+                            startActivity(new Intent(UserProfile.this, ViewClientAppointmentsActivity.class));
+                        }
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        //   Log.w(TAG, "Failed to read value.", error.toException());
+                    }
+                });
+            }
+        });
+
+
+
+
     }
 }
